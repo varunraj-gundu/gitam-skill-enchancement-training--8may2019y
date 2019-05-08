62db52499b9399729fa3f24783bc110aaf725b6d
varunraj-gundu <50130444+varunraj-gundu@users.noreply.github.com>
2019-05-08 15:40:16 +0530

    Add files via upload

---
+++ a.py
@@ -0,0 +1,7 @@
+
+# Hello World program in Python
+t1=(1,2,3)
+print(t1[2])
+print(t1[2]+t1[1])
+print(t1[len(t1)-1])
+print(str(t1[0])[1]+""+str(t1[1])[1])
---
+++ basic.py
@@ -0,0 +1,22 @@
+# Hello World program in Python
+    
+test=[10,20,30]
+print(test)
+test.append(10)
+print(test)
+print(len(test))
+print(test.pop(0))
+print(test)
+var=5
+var2=var*var
+var3=var2*var
+print(var3)
+print(len(str(var3)))
+print(len(str(var3)))
+var4=((var3**var3)**(var3))
+print(var4)
+type("var")
+print(type(var))
+string=["apple","banana","mango","grapes"]
+print(string[0:2])
+print(string[2::-2])
