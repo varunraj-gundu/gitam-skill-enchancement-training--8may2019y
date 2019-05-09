dd3a8dd156b0239a0c0493c05e2e3d0e28d76374
varunraj-gundu <50130444+varunraj-gundu@users.noreply.github.com>
2019-05-09 17:06:28 +0530

    Add files via upload

---
+++ programs done in terminal/new 2.py
@@ -0,0 +1,13 @@
+<html>
+<ol>
+<H1>Problem Set</H1>
+    <li>Design a procedure calculate the maximum, minimum and average of N numbers</li>
+<ol> 
+    <li>data( a[1,2,3,4,5] ) -> Max = 5, Min = 1, Avg = 3</li>
+</ol>
+    <li>Design a procedure to perform Linear search on list of N unsorted numbers. It take an array and the key element to be searched and returns the index of the element of key element if found. Else returns -1</li>
+<ol>
+     <li>LinearSearch( a[5,4,3,2,1], 4) -> 2</li>
+     <li>LinearSearch( a[90, 123, 324, 21, 56], 22) -> -1</li>
+</ol>
+</html>
